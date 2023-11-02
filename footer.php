f22818976219d456345387f8127f3de49100f37e
enrico fossati <>
2023-11-02 17:08:22 +0100

    modifiche Enrico

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <?php 
 	/* Copyright 2023 Neen S.p.A.*/
 ?>
-
+<div class="container footer">
+  <img src="assets/logo-neen-footer.png" class="logo-neen-footer">
+  <div class="text-footer">Cloud Maturity Assessment is a NEEN idea - P.IVA 04817410964 </div>
+</div>
 		<!-- Popper.js first, then Bootstrap JS -->
         <script src="js/popper.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
